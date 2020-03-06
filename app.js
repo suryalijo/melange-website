cb7cf760e3673b2a1c4a7a0d87f2d591b63569d3
Surya Vijayakumar <61888245+suryalijo@users.noreply.github.com>
2020-03-06 16:28:36 -0500

    Added button click functionality in js

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
+var pics = [
+	"imgs/kitty_bed.jpg",
+	"imgs/kitty_basket.jpg", 
+	"imgs/kitty_laptop.jpg",
+	"imgs/kitty_door.jpg",
+	"imgs/kitty_sink.jpg",
+	"imgs/kitty_wall.jpg"
+]
+
+var buttonToChangeimage = document.querySelector("button");
+var image = document.querySelector("img");
+
+let counter = 1;
+buttonToChangeimage.addEventListener("click", function(){
+	if(counter === 6){
+		counter=0;
+	}
+	image.src = pics[counter];
+	counter++;
+});
 
